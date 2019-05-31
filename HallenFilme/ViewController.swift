4ad6fe6f1e6d4e71b9aa8da589e915bde9ef2bef
andreaspohl <Andreas.g.pohl@gmail.com>
2019-05-31 13:27:59 +0200

    Initial Commit

---
+++ HallenFilme/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HallenFilme
+//
+//  Created by Andreas on 31.05.19.
+//  Copyright © 2019 Andreas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
